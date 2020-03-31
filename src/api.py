1f4a899b80dd3137a9f3530fe87064166eaaea93
aashisharcade@gmail.com <aashisharcade@gmail.com>
2020-03-31 07:32:24 +0530

    layout created

--- src/api.py
+++ src/api.py
@@ -1,1 +1,2 @@
-key = '$2a$10$O8ny/oNpOjI2yXLWHkBOiOt/DnrEBBXdB0bzehst7fcMHP5tzUP82'
+api_key = '$2a$10$O8ny/oNpOjI2yXLWHkBOiOt/DnrEBBXdB0bzehst7fcMHP5tzUP82'
+url = f'https://www.potterapi.com/v1/{category}?key={api_key}'
