555d34f257746b804100c00cac737ca16414778b
Aashish Koshti <aashisharcade@gmail.com>
2020-03-31 23:56:51 +0530

    worked api

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, url_for, redirect, session, request
 from flask_sqlalchemy import SQLAlchemy
+import json
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+access_token = '503983410507122'
+
+@app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        get_name = request.form['typehero']
+        base_url = f'https://superheroapi.com/api/{access_token}/search/{get_name}'
+        r = requests.get(base_url)
+        data = r.json()
+        results = data['results'][0]
+        return render_template('index.html',name = results['name'])
+    else:
+        return render_template('index.html',name = '')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
--- src/api.py
+++ src/api.py
@@ -1,2 +1,18 @@
-api_key = '$2a$10$O8ny/oNpOjI2yXLWHkBOiOt/DnrEBBXdB0bzehst7fcMHP5tzUP82'
-url = f'https://www.potterapi.com/v1/{category}?key={api_key}'
+import requests
+import json
+
+access_token = '503983410507122'
+name = 'superman'
+base_url = f'https://superheroapi.com/api/{access_token}/search/{name}'
+
+r = requests.get(base_url)
+data = r.json()
+results = data['results'][0]
+print(results.keys())
+print(results['name'])
+# print(results['powerstats'])
+# print(results['biography'])
+# print(results['appearance'])
+# print(results['work'])
+# print(results['connections'])
+# print(results['image'])
